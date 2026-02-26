8a9d386fcbd7ceafa47bdaa258351d2527b89816
LechuDev <mexacode@outlook.com>
2026-02-26 00:09:08 -0600

    Changes to be committed: 	modified:   Ejercicios/15_AplicaciondeIVA.c 	new file:   Ejercicios/16_Calificaciones.c 	new file:   Ejercicios/17_ConversionaSegundos.c 	new file:   Ejercicios/18_MediaGeometrica.c 	new file:   Ejercicios/19_Ciclo_If.c 	modified:   README.md

--- Ejercicios/15_AplicaciondeIVA.c
+++ Ejercicios/15_AplicaciondeIVA.c
@@ -1,12 +1,12 @@
-//Crear una aplicación que calcule el IVA de un producto dado su precio y la tasa de IVA. y te lo de su precio final con IVA incluido.
 #include <stdio.h>
 int main(){
-    float precio, iva, precioFinal;
+    float precio, iva, precioFinal, aumento;
     printf("Ingrese el precio del producto: ");
     scanf("%f", &precio);
     printf("Ingrese la tasa de IVA: ");
     scanf("%f", &iva);
-    precioFinal = precio + (precio * iva / 100);
+    aumento = precio * iva / 100;
+    precioFinal = precio + aumento;
     printf("El precio final del producto con IVA incluido es: %.2f\n", precioFinal);
     return 0;
 }
